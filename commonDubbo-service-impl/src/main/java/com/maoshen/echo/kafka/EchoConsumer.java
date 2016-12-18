1eb712492f438fc76d23a8fad76aa7ca9d81d872
15914380151@139.com <15914380151@139.com>
2016-12-18 18:59:30 +0800

    KAFKA重发机制

--- commonDubbo-service-impl/src/main/java/com/maoshen/echo/kafka/EchoConsumer.java
+++ commonDubbo-service-impl/src/main/java/com/maoshen/echo/kafka/EchoConsumer.java
@@ -1,29 +1,50 @@
 package com.maoshen.echo.kafka;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.maoshen.component.kafka.BaseConsumer;
+import com.maoshen.component.kafka.BaseProducer;
 import com.maoshen.component.kafka.dto.MessageDto;
 import com.maoshen.component.kafka.dto.MessageVo;
 
 @Service
 public class EchoConsumer extends BaseConsumer {
+	@Autowired
+	@Qualifier("baseProducer")
+	private BaseProducer baseProducer;
+	
 	private static final Logger LOGGER = Logger.getLogger(BaseConsumer.class);
 	public EchoConsumer(){
 		super();
 	}
 	@Override
 	public void onMessage(MessageDto dto) {
-		LOGGER.info("EchoConsumer receive message start,value is:" + JSONObject.toJSONString(dto) + ",time is:" + new Date());
-		LOGGER.info("EchoConsumer receive message end,time is:"+ new Date());
+		LOGGER.error("EchoConsumer receive message start,value is:" + JSONObject.toJSONString(dto) + ",time is:" + new Date());
+		LOGGER.error("EchoConsumer receive message end,time is:"+ new Date());
+		
+		Map<String,Object> sendMapSub = new HashMap<String,Object>();
+		sendMapSub.put("jdxSub", UUID.randomUUID().toString());
+		baseProducer.send(MessageVo.ECHO_MESSAGE_SUB.getTopicName(),sendMapSub,dto.getRequestId());
+		
+		String s = "f";
+		int i = Integer.parseInt(s);
 	}
 
 	@Override
 	public MessageVo getGroupIdANdTopicName() {		
 		return MessageVo.ECHO_MESSAGE;
+	}
+	@Override
+	public boolean isResend() {
+		return true;
 	}
 }
---
+++ commonDubbo-service-impl/src/main/java/com/maoshen/echo/kafka/EchoConsumerSub.java
@@ -0,0 +1,35 @@
+package com.maoshen.echo.kafka;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.maoshen.component.kafka.BaseConsumer;
+import com.maoshen.component.kafka.dto.MessageDto;
+import com.maoshen.component.kafka.dto.MessageVo;
+
+@Service
+public class EchoConsumerSub extends BaseConsumer {
+	private static final Logger LOGGER = Logger.getLogger(BaseConsumer.class);
+	public EchoConsumerSub(){
+		super();
+	}
+	@Override
+	public void onMessage(MessageDto dto) {
+		LOGGER.error("EchoConsumerSub receive message start,value is:" + JSONObject.toJSONString(dto) + ",time is:" + new Date());
+		LOGGER.error("EchoConsumerSub receive message end,time is:"+ new Date());
+		String s = "f";
+		//int i = Integer.parseInt(s);
+	}
+
+	@Override
+	public MessageVo getGroupIdANdTopicName() {		
+		return MessageVo.ECHO_MESSAGE_SUB;
+	}
+	@Override
+	public boolean isResend() {
+		return true;
+	}
+}
